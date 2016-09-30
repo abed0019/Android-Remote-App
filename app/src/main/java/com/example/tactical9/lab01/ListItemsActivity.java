182b7e5726f7ac6cb4bf04dba99a06b72f284617
abed0019 <zobayedzeeshan@yahoo.com>
2016-09-30 10:33:10 -0400

    LAB 3 done (except last step)

--- app/src/main/java/com/example/tactical9/lab01/ListItemsActivity.java
+++ app/src/main/java/com/example/tactical9/lab01/ListItemsActivity.java
@@ -1,29 +1,168 @@
 package com.example.tactical9.lab01;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.LauncherActivity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ListItemsActivity extends AppCompatActivity {
+
+
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
+    ImageButton imgButton;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    // Button imageButton;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // final int REQUEST_IMAGE_CAPTURE = 1;
+
+        //public void dispatchTakePictureIntent() {
+         //   Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+         //   if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+          //      startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+          //  }
+      //  }
+
+        imgButton = (ImageButton) findViewById(R.id.imageButton);
+        imgButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    }
             }
         });
+
+        Switch flipper = (Switch) findViewById(R.id.switch1);
+        flipper.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    CharSequence text = "Switch is ON";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(ListItemsActivity.this, text, duration);
+                    toast.show();
+                   // Toast.makeText(getApplicationContext(),"Switch is On", Toast.LENGTH_LONG.show());
+                }else{
+                    CharSequence text = "Switch is OFF";
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(ListItemsActivity.this, text, duration);
+                    toast.show();
+                    //Toast.makeText(getApplicationContext(),"Switch is Off", Toast.LENGTH_LONG.show());
+                }
+            }
+        });
+
+
+        CheckBox box = (CheckBox) findViewById(R.id.checkBox);
+        box.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (isChecked){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+// 2. Chain together various setter methods to set the dialog characteristics
+                    builder.setMessage(R.string.dialog_message) //Add a dialog message to strings.xml
+
+                            .setTitle(R.string.dialog_title)
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+
+
+                                    //NOT SURE WHAT IS GOING ON HERE ( STEP 10 - NEED HELP )
+
+                                    Intent resultIntent = new Intent();
+                                    resultIntent.putExtra("Response", "My information to share");
+                                    setResult(Activity.RESULT_OK, resultIntent);
+                                    finish();
+
+                                }
+                            })
+                            .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    // User cancelled the dialog
+                                }
+                            })
+                            .show();
+
+                }
+
+            }
+        });
+
+
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgButton.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME,"In onResume()");
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME,"In onStart()");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME,"In onPause)");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME,"In onStop()");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
+    }
 }
--- app/src/main/java/com/example/tactical9/lab01/LoginActivity.java
+++ app/src/main/java/com/example/tactical9/lab01/LoginActivity.java
@@ -1,29 +1,90 @@
 package com.example.tactical9.lab01;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
+    protected static final String ACTIVITY_NAME = "LoginActivity";
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*RelativeLayout layout = new RelativeLayout(this);
+        Button button = new Button(this);
+        layout.addView(button);
+        setContentView(layout);*/
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+final EditText nameField = (EditText)findViewById(R.id.editText);
+        //open the file
+        final SharedPreferences prefs = getSharedPreferences("storeddata", Context.MODE_PRIVATE);
+        final String storedName = prefs.getString("LoginName", "email@domain.com");
+
+
+nameField.setText(storedName);
+
+        loginButton = (Button)findViewById(R.id.loginbutton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                //Get an object to write to the file:
+                SharedPreferences.Editor writer = prefs.edit();
+
+                //Write put a number:
+                writer.putString("LoginName", nameField.getText().toString()  );
+
+                //Write the file:
+                writer.commit();
+LoginActivity.this.startActivity(new Intent( LoginActivity.this, StartActivity.class   ));
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME,"In onResume()");
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME,"In onStart()");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME,"In onPause()");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME,"In onStop()");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
     }
 
 }
